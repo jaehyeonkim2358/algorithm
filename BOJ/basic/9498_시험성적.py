6566486d5816c970b820e5fddac23951e0ac282f
Jaehyeon <jaehyeonkim2358@gmail.com>
2022-09-08 00:53:05 +0900

    Add new file

---
+++ BOJ/basic/9498_시험성적.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def main():
+    score = int(sys.stdin.readline())
+    grade = 'F'
+    if 100 >= score >= 90:
+        grade = 'A'
+    elif 89 >= score >= 80:
+        grade = 'B'
+    elif 79 >= score >= 70:
+        grade = 'C'
+    elif 69 >= score >= 60:
+        grade = 'D'
+
+    sys.stdout.write(grade)
+
+
+if __name__ == '__main__':
+    main()
